33e8990e218bebb964fa96eecdd5875de8e32fa3
Koen van Gilst <koen.vangilst@gmail.com>
2019-12-23 20:59:27 +0100

    initial commit of datalist implementation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { countries } from "./data";
 
 function App() {
+  const [country, setCountry] = React.useState("");
+  const [number, setNumber] = React.useState("");
+  const phone = country + number;
+  const regex = /^\+(?:[0-9] ?){6,14}[0-9]$/;
+  const invalid = !regex.test(phone);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    alert(`You entered ${phone}`);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <fieldset>
+        <legend>Phone number</legend>
+        <label htmlFor="country-code">Country</label>
+        <input
+          placeholder="+44"
+          type="text"
+          list="countrycodes"
+          id="country-code"
+          value={country}
+          onChange={e => setCountry(e.target.value)}
+          aria-invalid={invalid}
+        />
+        <datalist id="countrycodes">
+          {countries.map(country => {
+            return (
+              <option key={country.code} value={`+${country.phone}`}>
+                {country.label} (+{country.phone})
+              </option>
+            );
+          })}
+        </datalist>
+        <label htmlFor="number">Number</label>
+        <input
+          id="number"
+          type="phone"
+          placeholder="12345678"
+          value={number}
+          onChange={e => setNumber(e.target.value)}
+          aria-invalid={invalid}
+        />
+        <div className={`message ${invalid ? "message--invalid" : ""}`}>
+          {invalid ? "Invalid phone number" : ""}
+        </div>
+        <input type="submit" value="Submit" disabled={invalid} />
+      </fieldset>
+    </form>
   );
 }
 
